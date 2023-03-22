e645a1c1677b1df2c5e0324886d4cf9d4d4d0fbc
jzamorano20 <126306433+jzamorano20@users.noreply.github.com>
2023-03-22 10:36:04 -0400

    tracking

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.modal').modal();
+
+    //listen for submit on the search bar (event listener "submit")
+    // when event happens the do $.get(dictionaryBaseUrl + searchTeamFromSearchBar)
+    // then with data put on page
+  });
